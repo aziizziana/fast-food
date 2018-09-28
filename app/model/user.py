320c23cc48f06c7b424387aeaf53003a2215b7d2
Ziana <aziizziana@gmail.com>
2018-09-28 20:31:30 +0300

    Add header and footer

--- app/model/user.py
+++ app/model/user.py
@@ -1,7 +1,6 @@
-import jwt
-import datetime
-from config import get_env
-class user:
+
+class User:
+
     def __init__(self, first_name, last_name, email, password, admin = False):
         self.first_name = first_name
         self.last_name = last_name
@@ -9,25 +8,4 @@ class user:
         self.password = password
         self.admin = admin
 
-    def get_token(self):
-        token = jwt.encode({
-            'first_name': self.first_name,
-            'last_name': self.last_name,
-            'email': self.email,
-            'admin': self.admin,
-            'iat': datetime.datetime.utcnow(),
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=get_env('AUTH_TOKEN_EXPIRY_DAYS'),
-                                                                   seconds=get_env('AUTH_TOKEN_EXPIRY_SECONDS')),
-
-        }, get_env('APP_SECRET'), algorithm='HS256')
-        
-
-    @staticmethod
-    def decode(self, token):
-        try:
-            payload = jwt.decode(token, get_env['APP_SECRET'], algorithms='HS256')
-            return payload['email']
-        except jwt.ExpiredSignatureError:
-            return 'signature expired'
-        except jwt.InvalidTokenError:
-            return 'Invalid Token'
+    
